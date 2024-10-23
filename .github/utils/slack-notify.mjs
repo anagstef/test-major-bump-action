89df3564c38e84612be478fccb45d5396d489c87
LekoArts <lekoarts@gmail.com>
2024-10-23 11:45:08 -0400

    start debugging slack notif

---
+++ .github/utils/slack-notify.mjs
@@ -0,0 +1,76 @@
+/**
+ * @typedef {Object} Actor
+ * @property {string} username
+ * @property {string} avatarUrl
+ * @property {string} profileUrl
+ */
+
+/**
+ * @typedef {Object} PRData
+ * @property {string} title
+ * @property {string} body
+ * @property {Actor} actor
+ */
+
+/**
+ * @typedef {Object} Formatter
+ * @property {(data: PRData) => string} generateMsg
+ */
+
+/**
+ * Slack is using their own Markdown format, see:
+ * https://api.slack.com/reference/surfaces/formatting#basics
+ * https://app.slack.com/block-kit-builder
+ * @type {Formatter}
+ */
+const slackFormatter = {
+  generateMsg: ({ title, body, actor }) => {
+    const markdown = text => ({ type: 'section', text: { type: 'mrkdwn', text } });
+    const divider = () => ({ type: 'divider' });
+    const header = text => ({ type: 'header', text: { type: 'plain_text', text } });
+    const context = (imgUrl, text) => ({
+      type: 'context',
+      elements: [
+        ...(imgUrl ? [{ type: 'image', image_url: imgUrl, alt_text: 'avatar' }] : []),
+        { type: 'mrkdwn', text },
+      ],
+    });
+    const blocks = [];
+
+    blocks.push(header(title));
+
+    blocks.push(markdown(body));
+    blocks.push(markdown('\n'));
+    blocks.push(context(actor.avatarUrl, `<${actor.profileUrl}|*${actor.username}*> created this PR.`));
+
+    return JSON.stringify({ blocks });
+  },
+};
+
+/**
+ * @type {Record<string, Formatter>}
+ */
+const formatters = {
+  slack: slackFormatter,
+};
+
+const run = async () => {
+  const { title, body, user } = JSON.parse(process.argv[2]);
+  const actor = createActor(user.login);
+
+  const formatter = formatters['slack'];
+  if (!formatter) {
+    throw new Error('Invalid formatter, supported formatters are: ' + Object.keys(formatters).join(', '));
+  }
+
+  console.log(formatter.generateChangelog({ title, body, actor }));
+}
+
+run()
+
+/*
+ * @returns {Actor}
+ */
+const createActor = username => {
+  return { username, avatarUrl: `https://github.com/${username}.png`, profileUrl: `https://github.com/${username}` };
+};
